92363b170f7c98cd81612dbbfe1ea0253383110f
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-28 11:54:49 +0500

    Fix database model imports

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from config import settings # yoki DB_URL qayerdan olinsa
 
-# Asinxron dvigatelni yaratish
-engine = create_async_engine(settings.DB_URL, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+from config import settings
+from app.models import Base
+
+
+engine = create_async_engine(
+    settings.DB_URL,
+    echo=True,
+)
+
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
+
 
 async def init_db():
-    # Jadvallarni yaratish
-    from app.database.models import Base
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
