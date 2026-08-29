a4e8b749a53507141904d188fa56d136bee6db97
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-29 13:37:16 +0500

    Render uchun yangilandi va xatolar tuzatildi

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,18 +1,29 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DB_URL") or os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL topilmadi")
+    raise RuntimeError("DATABASE_URL yoki DB_URL topilmadi .env faylida!")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=True,
     pool_pre_ping=True,
-    pool_recycle=300,
-    connect_args={
-        "ssl": "require",
-        "timeout": 30,
-    },
+    echo=False
 )
+
+async_session = async_sessionmaker(
+    engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def init_db():
+    async with engine.begin() as conn:
+        pass
