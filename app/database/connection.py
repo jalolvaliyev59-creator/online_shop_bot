af1e10e0042de71c2d136b84b62635b02eacded5
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-29 12:59:08 +0500

    PostgreSQL SSL va pool_pre_ping tuzatildi

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,17 +1,23 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.database.base import Base
 from config import settings
-from app.models import Base
+
+connect_args = {}
+if "postgresql" in settings.DB_URL:
+    connect_args = {"ssl": "require"}
 
 engine = create_async_engine(
-    settings.DB_URL,  # <-- DATABASE_URL emas, aynan DB_URL bo'lishi kerak
-    echo=True, 
-    connect_args={"ssl": "require"} 
+    settings.DB_URL,
+    echo=False,
+    pool_pre_ping=True,
+    connect_args=connect_args
 )
 
 async_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
 )
+
 
 async def init_db():
     async with engine.begin() as conn:
