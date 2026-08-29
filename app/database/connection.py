c57649ec1333308f1a8685163435359025c9f433
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-29 13:05:58 +0500

    fix postgres connection

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,22 +1,10 @@
-
 import os
 from sqlalchemy.ext.asyncio import create_async_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL Render Environment Variables'da topilmadi")
-
-# Render DATABASE_URL:
-# postgresql://...
-# SQLAlchemy async uchun:
-# postgresql+asyncpg://...
-if DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgresql://",
-        "postgresql+asyncpg://",
-        1
-    )
+    raise RuntimeError("DATABASE_URL topilmadi")
 
 engine = create_async_engine(
     DATABASE_URL,
