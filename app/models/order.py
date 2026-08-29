3535b9d1618de177ae96bb4974ce941e07739b72
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-29 10:44:56 +0500

    BigInteger xatosi tuzatildi

--- app/models/order.py
+++ app/models/order.py
@@ -2,13 +2,15 @@ from datetime import datetime
 from sqlalchemy import DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.base import Base
+from sqlalchemy import DateTime, ForeignKey, Integer, BigInteger, String, func
+
 
 
 class Order(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, nullable=False, index=True)
     shop_id: Mapped[int] = mapped_column(ForeignKey("shops.id"), nullable=False)
     total_price: Mapped[int] = mapped_column(Integer, nullable=False)
     status: Mapped[str] = mapped_column(String(30), default="new", nullable=False)
--- app/models/wishlist.py
+++ app/models/wishlist.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Integer, UniqueConstraint
+from sqlalchemy import ForeignKey, Integer, BigInteger, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database.base import Base
 
@@ -7,7 +7,7 @@ class Wishlist(Base):
     __tablename__ = "wishlist"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, nullable=False, index=True)
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
     shop_id: Mapped[int] = mapped_column(ForeignKey("shops.id"), nullable=False)
 
