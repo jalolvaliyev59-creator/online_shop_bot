58cfdc355774117958480c7b767f5ac7ef18fa10
Sybill Danna <sizning_emailingiz@gmail.com>
2026-08-29 11:52:07 +0500

    Connection.py da settings.DATABASE_URL ishlatildi

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,21 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
 from config import settings
 from app.models import Base
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-
+# settings ichidan DATABASE_URL ni olib ishlatamiz
 engine = create_async_engine(
-    DATABASE_URL, 
+    settings.DATABASE_URL, 
     echo=True, 
     connect_args={"ssl": "require"} 
 )
+
 async_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
 )
-
 
 async def init_db():
     async with engine.begin() as conn:
